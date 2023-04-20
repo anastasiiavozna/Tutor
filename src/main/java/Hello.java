3fcb7ba2c5aea68afc9d6bdf5232dc9227f6315f
anastasiiavozna <Avozna777@gmail.com>
2023-04-19 21:14:37 -0400

    first commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    public void str (String str){
+        System.out.println(str);
+            }
+
+    public static void main(String[] args) {
+        Hello obj=new Hello();
+        String tutor="Hello world";
+        obj.str(tutor);
+    }
+}
